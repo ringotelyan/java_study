da02edf9bd8344b529da87be0167d0ce1b55e1dc
ringotelyan <103027980+ringotelyan@users.noreply.github.com>
2022-07-24 23:35:18 +0900

    [Finish]第7章

--- JavaWcp/src/If02.java
+++ JavaWcp/src/If02.java
@@ -2,12 +2,26 @@
 public class If02 {
 	public static void main(String[] args) {
 		int number = 7;
+		// if文：
+		// numberが5未満の場合、ブロックの処理を実行
 		if (number < 5) {
+			// ブロック：「〇は5未満です」を出力
 			System.out.println(number + " は5未満です");
+			// else-if文
+			// 前のif文、-の条件式の評価が全てfalseで、
+			// numberが10未満の場合、ブロックの処理尾w実行
 		} else if (number < 10) {
+			// 評価がtrueなので、処理が実行される
+			// ブロック：「〇は5以上、10未満です」を出力
 			System.out.println(number + " は5以上、10未満です");
+			// else-if文：
+			// 前の条件式の評価が全てfalseで、numberが15未満の場合、ブロックの処理を実行
 		} else if (number < 15) {
+			// 表かはtrueだが、前の評価がtrueのため実行されない
+			// ブロック：「〇は10以上、15未満です」を出力
 			System.out.println(number + " は10以上、15未満です");
+			// else文：
+			// 前の条件式の評価が全てfalseの場合、ブロックの処理を実行
 		} else {
 			System.out.println(number + " は15未満ではないです");
 		}
