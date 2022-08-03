ef7c7155f60e20100eebcbd0a36b55aec57593b3
ringotelyan <103027980+ringotelyan@users.noreply.github.com>
2022-08-03 23:53:00 +0900

    [Update]第１２章

--- JavaWcp/src/capsule/human/HumanNoCapsule.java
+++ JavaWcp/src/capsule/human/HumanNoCapsule.java
@@ -1,24 +1,13 @@
 package capsule.human;
 
 public class HumanNoCapsule {
+	// publicのアクセス修飾子のため外部から参照が可能
 	public String name;
 	public int age;
 	
 	public HumanNoCapsule(String name, int age) {
 		this.name = name;
 		this.age = age;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getAge() {
-		return this.age;
 	}
 
 }
