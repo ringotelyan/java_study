c483330170c372290dfd85374c80bd8e9b2c8317
ringotelyan <103027980+ringotelyan@users.noreply.github.com>
2022-08-06 23:21:31 +0900

    [Start]第13章

--- JavaWcp/src/extend/human/Student.java
+++ JavaWcp/src/extend/human/Student.java
@@ -1,5 +1,22 @@
 package extend.human;
 
-public class Student {
+// Humanクラスを継承したStudenクラス
+public class Student extends Human {
+	// サブクラスで追加したフィールド
+	private int score;
+	
+	public Student(String name, int age, int score) {
+		// スーパークラスのコンストラクタを呼び出し
+		super(name, age);
+		this.score = score;
+	}
+	
+	// サブクラスで追加したメソッド
+	public String getStrudentProfile() {
+		// 親インスタンスのフィールドをsuper変数で参照
+		String profile = "年齢は" + super.age + "です。";
+		profile += "学生で、テストの点数は" + this.score + "点です。";
+		return profile;
+	}
 
 }
