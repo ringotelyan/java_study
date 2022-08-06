c254ce2bed7fe981a23d9b5b5539738f988a717a
ringotelyan <103027980+ringotelyan@users.noreply.github.com>
2022-08-06 23:24:34 +0900

    [Start]第13章

--- JavaWcp/src/extend/human/Employee.java
+++ JavaWcp/src/extend/human/Employee.java
@@ -1,5 +1,22 @@
 package extend.human;
 
-public class Employee {
+// Humanクラスを継承したEmployee暮らす
+public class Employee extends Human {
+	// サブクラスで追加したフィールド
+	private String department;
+	
+	public Employee(String name, int age, String department) {
+		// スーパークラスのコンストラクタを呼び出し
+		super(name, age);
+		this.department = department;
+	}
+	
+	// サブクラスで追加したメソッド
+	public String getEmployeeProfile() {
+		// 親インスタンスのフィールドをthis変数で参照
+		String profile = "年齢は" + this.age + "です。";
+		profile += "サラリーマンで、部署は" + this.department + "です。";
+		return profile;
+	}
 
 }
