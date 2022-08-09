32a15d18e118efdf217c2f9b0980e970e78c382d
ringotelyan <103027980+ringotelyan@users.noreply.github.com>
2022-08-09 17:56:46 +0900

    [Update]第14章

--- JavaWcp/src/polymorphism/human/Student.java
+++ JavaWcp/src/polymorphism/human/Student.java
@@ -1,5 +1,20 @@
 package polymorphism.human;
 
-public class Student {
+// Humanクラスを継承
+public class Student extends Human {
+	private Student(String name, int age, int score) {
+		// スーパークラスのコンストラクタを呼び出し
+		super(name, age);
+		this.score = score;
+	}
+	
+	// オーバーライドして、Studentクラス用に処理を変更
+	@Override
+	public String getProfile() {
+		String profile = "年齢は" + super.age + "です。";
+		prifile += "学生で、テストの点数は" + this.score + "点です。";
+		return profile;
+		}
+	}
 
 }
