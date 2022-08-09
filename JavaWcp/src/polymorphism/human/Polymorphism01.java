a0a4faddb06ad0526419c3b0322edd7e02df9f87
ringotelyan <103027980+ringotelyan@users.noreply.github.com>
2022-08-09 18:08:25 +0900

    [Update]第14章

--- JavaWcp/src/polymorphism/human/Polymorphism01.java
+++ JavaWcp/src/polymorphism/human/Polymorphism01.java
@@ -1,5 +1,16 @@
 package polymorphism.human;
 
 public class Polymorphism01 {
+	public static void main(String[] args) {
+		// Studentクラスのインスタンスを生成して、Humanクラスの変数に代入
+		Human human1 = new Student("佐藤", 17, 70);
+		// オーバーライドしているので、Studentクラスのメソッドが実行される
+		System.out.println("Humanクラスのメソッド：プロフィールを紹介します。" + human1.getProfile());
+		
+		// Employeeクラスのインスタンスを生成して、Humanクラスの変数に代入
+		Human human2 = new Employee("田中", 28, "システム部");
+		// オーバーライドしているので、Employeeクラスのメソッドが実行される
+		System.out.println("Humanクラスのメソッド：プロフィールを紹介します。" + human2.getProfile());
+	}
 
 }
